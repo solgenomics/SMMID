201b7eb7ac5c069c0de4cd355e8cae79b2b668f0
Production User <production@smid-db.sgn.cornell.edu>
2020-09-11 19:40:06 -0400

    add a dependency build script.

---
+++ build.sh
@@ -0,0 +1,15 @@
+
+cpanm -L local-lib Catalyst
+cpanm -L ../local-lib/ Catlyast::Plugin::Static::Simple
+cpanm -L ../local-lib/ Catalyst::Plugin::Static::Simple
+cpanm -L ../local-lib/ Catalyst::Plugin::ConfigLoader
+cpanm -L ../local-lib/ Catalyst::Restarter
+cpanm -L ../local-lib/ Catalyst::Plugin::Authorization::Roles
+cpanm -L ../local-lib/ String::Random
+cpanm -L ../local-lib/ DateTime
+cpanm -L ../local-lib/ DateTime::Format::ISO8601
+cpanm -L ../local-lib/ Catalyst::Plugin::SmartURI
+cpanm -L ../local-lib/ Catalyst::Model::DBIC::Schema
+cpanm -L ../local-lib/ Catalyst::View::HTML::Mason
+cpanm -L ../local-lib/ Tie::UrlEncoder
+cpanm -L ../local-lib/ DBD::Pg
