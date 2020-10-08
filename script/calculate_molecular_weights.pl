12183c56ab76d080e9a71cfd5c740b56d6ce1d0a
Lukas Mueller <lam87@cornell.edu>
2020-10-08 13:45:04 -0400

    add search interface, experiment detail mason, script to calculate MWs.

---
+++ script/calculate_molecular_weights.pl
@@ -0,0 +1,37 @@
+
+use strict;
+
+use SMIDDB;
+
+my $schema = SMIDDB->connect('dbi:Pg:dbname=smid_db;host=breedbase_db;user=postgres;password=postgres');
+
+my $compounds = $schema->resultset("SMIDDB::Result::Compound")->search( {} );
+
+while (my $compound = $compounds->next()) {
+    my $molecular_weight = molecular_weight($compound->formula());
+
+    $compound->update( { molecular_weight => $molecular_weight });
+    
+    
+}
+
+
+
+sub molecular_weight {
+  #...
+  #The default variable will be used as the chemical Formula
+  $_ = shift(@_);
+
+  my %elements = ("H" => 1.01, "C" => 12.01, "O" => 16.0, "N" => 14.01, "P" => 30.97, "S" => 32.06);
+  my $weight = 0;
+
+  my @pairs = /([CHONPS][0-9]*)/g;
+  foreach my $pair (@pairs){
+    if (length($pair)==1){
+      $weight += $elements{substr($pair, 0, 1)};
+    }else{
+      $weight += $elements{substr($pair, 0, 1)}*substr($pair, 1);
+    }
+  }
+  return $weight;
+}
