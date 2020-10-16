11532c6ac7f9c159ea8b12bbe54ba2a451cd0f75
Production User <production@smid-db.sgn.cornell.edu>
2020-10-16 14:02:22 -0400

    add missing dep DateTime::Format::Pg

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cpanm -L /home/production/local-lib/ Catalyst::Plugin::Authorization::Roles
 cpanm -L /home/production/local-lib/ String::Random
 cpanm -L /home/production/local-lib/ DateTime
 cpanm -L /home/production/local-lib/ DateTime::Format::ISO8601
+cpanm -L /home/production/local-lib/ DateTime::Format::Pg
 cpanm -L /home/production/local-lib/ Catalyst::Plugin::SmartURI
 cpanm -L /home/production/local-lib/ Catalyst::Model::DBIC::Schema
 cpanm -L /home/production/local-lib/ Catalyst::View::HTML::Mason
