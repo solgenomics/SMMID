c1eac850404a2b45b303b70f28018d2a047de85d
Production User <production@smid-db.sgn.cornell.edu>
2020-10-16 11:23:10 -0400

    add missing deps to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 
 cpanm -L /home/production/local-lib Catalyst
-cpanm -L /home/production/local-lib/ Catlyast::Plugin::Static::Simple
 cpanm -L /home/production/local-lib/ Catalyst::Plugin::Static::Simple
 cpanm -L /home/production/local-lib/ Catalyst::Plugin::ConfigLoader
 cpanm -L /home/production/local-lib/ Catalyst::Restarter
@@ -14,6 +13,7 @@ cpanm -L /home/production/local-lib/ Catalyst::View::HTML::Mason
 cpanm -L /home/production/local-lib/ Tie::UrlEncoder
 cpanm -L /home/production/local-lib/ DBD::Pg
 cpanm -L /home/production/local-lib/ Try::Tiny
-cpanm -L /home/production/local-lib/ JSON::Any
+cpanm -L /home/production/local-lib/ JSON::XS
 cpanm -L /home/production/local-lib/ Chemistry::File::SMILES
+cpanm -L /home/production/local-lib/ Image::Size
 
