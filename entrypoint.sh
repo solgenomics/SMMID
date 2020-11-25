8909f26944573317da1d861f4bb05aeec91cabcd
Lukas Mueller <lam87@cornell.edu>
2020-11-24 22:26:03 -0500

    add a docker implementation of the site.

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+/etc/init.d/cron start
+
+cd /home/production/SMMID/root/js
+
+npm install
+
+perl /home/production/SMMID/script/smmid_server.pl -p 8088
+#tail -f /home/production/SMMID/script/smmid_server.pl
