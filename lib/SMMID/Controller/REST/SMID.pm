c391cba65bab52340d2c2d93dc48223186c288aa
Ryan Preble <ryan@debian9.com>
2020-12-03 14:56:42 -0500

    Placed stylesheets in correct file and added timeout for msms spectrum mouseover

--- lib/SMMID/Controller/REST/SMID.pm
+++ lib/SMMID/Controller/REST/SMID.pm
@@ -597,7 +597,17 @@ sub results : Chained('smid') PathPart('results') Args(0) {
 	if ($experiment_type eq "ms_spectrum") {
 	    my $json = $row->data();
 	    my $hash = JSON::XS->new()->decode($json);
-	    push @data, [ $hash->{ms_spectrum_author}, $hash->{ms_spectrum_ionization_mode}, $hash->{ms_spectrum_collision_energy}, $hash->{ms_spectrum_adduct_fragmented}, "<a href=\"/experiment/".$row->experiment_id()."\">Details</a> <br></br><a href=\"/experiment/".$row->experiment_id()."/msms_spectrum\" onmouseover=\"display_msms_visual(".$row->experiment_id().")\">Spectrum</a>", $hash->{ms_spectrum_link},  $delete_link ];
+      my $mouseover= "
+        var timer;
+        var delay = 1000;
+        \$(this).hover(function(){
+          timer=setTimeout(function(){
+            display_msms_visual(".$row->experiment_id().")
+          }, delay);
+        }, function(){
+          clearTimeout(timer);
+        });";
+	    push @data, [ $hash->{ms_spectrum_author}, $hash->{ms_spectrum_ionization_mode}, $hash->{ms_spectrum_collision_energy}, $hash->{ms_spectrum_adduct_fragmented}, "<a href=\"/experiment/".$row->experiment_id()."\">Details</a> <br></br><a href=\"/experiment/".$row->experiment_id()."/msms_spectrum\" onmouseover=\"".$mouseover."\">Spectrum</a>", $hash->{ms_spectrum_link},  $delete_link ];
 	}
     }
 
