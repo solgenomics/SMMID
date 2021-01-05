230dc87f86efdedca66321a70f43576669aec6c7
Ryan Preble <ryan@debian9.com>
2021-01-05 15:58:48 -0500

    Added files to git repo

---
+++ lib/SMMID/Controller/User.pm
@@ -0,0 +1,21 @@
+
+package SMMID::Controller::User;
+
+use Moose;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+sub user :Chained('/') PathPart('user') CaptureArgs(1){
+  my $self = shift;
+  my $c = shift;
+  $c->stash->{sp_person_id} = shift;
+}
+
+sub profile :Chained('user') PathPart(''){
+  my $self = shift;
+  my $c = shift;
+
+  $c->stash->{template} = '/user.mas';
+}
+
+'SMMID::Controller::User';
