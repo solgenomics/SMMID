24ab923f661519889fd104f72fb37dada6d6a6af
root <root@aa82e883af73>
2021-03-12 21:42:41 +0000

    wrap create statements in evals in patch for better error processing.

--- dbpatches/0001/AddGroupTables.pm
+++ dbpatches/0001/AddGroupTables.pm
@@ -6,15 +6,28 @@ use Moose;
 extends 'Dbpatch::PatchRoot';
 
 sub init_patch {
+    my $self = shift;
+    $self->name('AddGroupTables');
+    $self->description('Adds dbgroup etc.');
+
 }
 
 sub patch {
     my $self = shift;
 
-    $self->dbh()->do("create table dbgroup ( dbgroup_id serial primary key, name varchar(100), description text)");
+    eval { 
+	$self->dbh()->do("create table dbgroup ( dbgroup_id serial primary key, name varchar(100), description text)");
+    };
+    if ($@) { print STDERR "ERROR: $@\n"; }
 
-    $self->dbh()->do("create table dbuser_dbgroup ( dbuser_dbgroup_id serial primary key, dbuser_id references dbuser, dbgroup_id references dbgroup)");
+    eval { 
+	$self->dbh()->do("create table dbuser_dbgroup ( dbuser_dbgroup_id serial primary key, dbuser_id bigint references dbuser, dbgroup_id bigint references dbgroup)");
+    };
+    if ($@) { print STDERR "ERROR: $@\n"; }
 
-    $self->dbh()->do("alter table compound add column dbgroup_id bigint references dbgroup");
-    
+    eval { 
+	$self->dbh()->do("alter table compound add column dbgroup_id bigint references dbgroup");
+    };
+    if ($@) { print STDERR "ERROR: $@\n"; }
+
 }
